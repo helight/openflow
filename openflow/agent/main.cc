446ec83f4a36b2aa6ad243c21d1ffb1e11e0fc9b
helight_guest <helight.xu@gmail.com>
2014-06-04 19:43:06 +0800

    add blade compile conf file

---
+++ openflow/agent/main.cc
@@ -0,0 +1,13 @@
+// Copyright (c) 2014, HelightXu
+// Author: Zhwen Xu<HelightXu@gmail.com>
+// Created: 2014-05-05
+// Description:
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
---
+++ openflow/tools/deque_test.cc
@@ -0,0 +1,33 @@
+// Copyright (c) 2014, HelightXu
+// Author: Zhwen Xu<HelightXu@gmail.com>
+// Created: 2014-01-05
+// Description:
+//
+
+#include <iostream>
+#include <deque>
+
+void test()
+{
+    std::deque <char> test(5,'2');
+    test.push_back('1');
+    test.push_back('5');
+
+    std::cout << test.size() << std::endl;
+    std::cout << sizeof(test) << std::endl;
+    std::cout << test.max_size() << std::endl;
+
+    int count = test.size();
+    for (int i = 0; i < count; i++)
+    {
+        std::cout << "test.pop: " << test.front() << std::endl;
+        test.pop_front();
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    test();
+
+    return 0;
+}
