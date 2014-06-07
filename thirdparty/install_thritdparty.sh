66cc5dc82856abc735669cda916ff6336db68c39
helight_guest <helight.xu@gmail.com>
2014-06-07 16:42:21 +0800

    delete libev-4.15.tar.gz

--- thirdparty/install_thritdparty.sh
+++ thirdparty/install_thritdparty.sh
@@ -159,7 +159,6 @@ function install_all()
     install_boost boost boost_1_55_0.tar.gz
     install_tar_gz_package openssl openssl-1.0.1g.tar.gz "shared threads"
     install_tar_gz_package libevent libevent-2.0.21-stable.tar.gz
-    install_tar_gz_package libev libev-4.15.tar.gz
     install_tar_gz_package gflags gflags-2.0.tar.gz
     install_tar_gz_package glog glog-0.3.3.tar.gz
     install_thirft thirft thrift-0.9.1.tar.gz
