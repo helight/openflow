4f9c10e1e856dd0c30e711c19c90e592b2c11364
helight_guest <helight.xu@gmail.com>
2014-09-24 20:14:49 +0800

    add codeinteger for web

---
+++ openflow/web/application/controllers/main.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/main
+	 *	- or -
+	 * 		http://example.com/index.php/main/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+        // ["mode"];//=1表示实例化,2表示查看,4=组件开发
+        $data["mode"] = 2;
+		$this->load->view('editor', $data);
+	}
+
+    public function edit()
+    {
+        $data["mode"] = 4;
+		$this->load->view('editor', $data);
+    }
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
