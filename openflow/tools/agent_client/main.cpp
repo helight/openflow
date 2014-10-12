1c868b7b46776a07a028a39f580d0919e023ef72
kobemiller <koebmiller@126.com>
2014-10-12 11:03:22 +0800

    实现agent端task的异步处理，之前的代码先注释掉了，暂时不删除

--- openflow/tools/agent_client/main.cpp
+++ openflow/tools/agent_client/main.cpp
@@ -25,12 +25,12 @@ extern "C" int main(int argc, char *argv[])
     openflow::task_info task1;
     task1.task_id = 8;
     task1.task_name = "kobemiller";
-    task1.cmd = "ps;ls -e;date;who;pwd;";
+    task1.cmd = "for((a=0; a < 1000000; ++a));do echo $a;done;";
 
     openflow::task_info task2;
     task2.task_id = 10;
     task2.task_name = "kobe";
-    task2.cmd = "ps;ls;date;who;pwd;";
+    task2.cmd = "for((a=0; a < 10000; ++a));do echo $a;done;";
 
     openflow::task_info task3;
     task3.task_id = 5;
@@ -39,36 +39,39 @@ extern "C" int main(int argc, char *argv[])
 
     tools::agent_client::CTask Ctask;
     
-    int tmp = Ctask.receive_task();
-    if ( tmp != 0 )
-    {
-        LOG(ERROR) << "fail to receive task.";
-        return -1;
-    }
+    // int tmp = Ctask.receive_task();
+    // if ( tmp )
+    // {
+    //     LOG(ERROR) << "fail to receive task.";
+    //     return -1;
+    // }
 
     LOG(INFO) << "execute task...";
-    tmp = thrift_client_helper->execute_task(task1);
-    if ( tmp != 0 )
-    {
-        LOG(ERROR) << "fail to execute task.";
-        return -1;
-    }
+    bool tmp;
+    tmp = thrift_client_helper->receive_task(task3);
+    tmp = thrift_client_helper->receive_task(task1);
+    tmp = thrift_client_helper->receive_task(task2);
+    // if ( tmp != 0 )
+    // {
+    //     LOG(ERROR) << "fail to execute task.";
+    //     return -1;
+    // }
 
-    tmp = thrift_client_helper->execute_task(task2);
-    if ( tmp != 0 )
-    {
-        LOG(ERROR) << "fail to execute task.";
-        return -1;
-    }
+    // tmp = thrift_client_helper->execute_task(task2);
+    // if ( tmp != 0 )
+    // {
+    //     LOG(ERROR) << "fail to execute task.";
+    //     return -1;
+    // }
 
-    tmp = thrift_client_helper->execute_task(task3);
-    if ( tmp != 0 )
-    {
-        LOG(ERROR) << "fail to execute task.";
-        return -1;
-    }
+    // tmp = thrift_client_helper->execute_task(task3);
+    // if ( tmp != 0 )
+    // {
+    //     LOG(ERROR) << "fail to execute task.";
+    //     return -1;
+    // }
 
-    thrift_client_helper->show_running_task();
+    // thrift_client_helper->show_running_task();
 
     return 0;
 }
